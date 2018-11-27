5b1718308a56e2f1fabcbda76afea37cae0b8a1f
wanghao <491993108@qq.com>
2018-11-27 10:19:59 +0800

    init

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: wh
+ * Date: 2018/11/27
+ * Time: 上午10:17
+ */
+class index
+{
+    public function demo()
+    {
+        echo 'this is my number one composer test';
+    }
+}
